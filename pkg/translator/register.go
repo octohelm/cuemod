c5f36b324497fcb998ff177c4cfa3909414ec573
Morlay <morlay.null@gmail.com>
2021-03-25 16:48:17 +0800

    feat: add translator yaml

--- pkg/translator/register.go
+++ pkg/translator/register.go
@@ -4,4 +4,5 @@ import (
 	_ "github.com/octohelm/cuemod/pkg/translator/helm"
 	_ "github.com/octohelm/cuemod/pkg/translator/jsonnet"
 	_ "github.com/octohelm/cuemod/pkg/translator/toml"
+	_ "github.com/octohelm/cuemod/pkg/translator/yaml"
 )
